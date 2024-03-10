47c8815c998935e3bd3a9e8eaea2e87f21cf760e
petermbachu.bincom <petermbachu.bincom@gmail.com>
2024-03-10 20:07:37 +0100

    updated sync and async files: terminal

--- paystackease/apis/terminal.py
+++ paystackease/apis/terminal.py
@@ -1,4 +1,6 @@
-""" Wrapper for Paystack Terminal APIs
+"""
+Wrapper for Paystack Terminal APIs
+
 The Terminal API allows you to build delightful in-person payment experiences.
 """
 
@@ -7,24 +9,27 @@ from paystackease.apis.base import PayStackBaseClientAPI
 
 
 class TerminalClientAPI(PayStackBaseClientAPI):
-    """Paystack Terminal API
+    """
+    Paystack Terminal API
     Reference: https://paystack.com/docs/api/terminal/
     """
 
     def send_event(
-        self,
-        terminal_id: str,
-        event_type: str,
-        terminal_action: str,
-        data_object: Dict[str, str],
+            self,
+            terminal_id: str,
+            event_type: str,
+            terminal_action: str,
+            data_object: Dict[str, str],
     ) -> dict:
-        """Send an event from your application to the Paystack Terminal
-        :param terminal_id: The terminal iD the event is sent to
-        :param event_type: The type of event to send. We currently support [ invoice | transaction ]
-        :param terminal_action: The action to perform on the terminal
+        """
+        Send an event from your application to the Paystack Terminal
+
+        :param: terminal_id: The terminal iD the event is sent to
+        :param: event_type: The type of event to send. We currently support [ invoice | transaction ]
+        :param: terminal_action: The action to perform on the terminal
         [invoice type]:the action can either be [ process || view ]
          [transaction type], the action can either be [ process || print ].
-         :param data_object: parameters needed to perform the specified action.
+         :param: data_object: parameters needed to perform the specified action.
          [invoice type]: you need to pass {id: invoice_id, reference: offline_reference}.
          [transaction type], you can pass {id: transaction_id}
 
@@ -35,8 +40,10 @@ class TerminalClientAPI(PayStackBaseClientAPI):
         return self._post_request(f"/terminal/{terminal_id}/event", data=data)
 
     def commission_terminal(self, serial_number: str) -> dict:
-        """Activate debug device by linking it to your integration
-        :param serial_number: The serial number of the device
+        """
+        Activate debug device by linking it to your integration
+
+        :param: serial_number: The serial number of the device
 
         :return: The response from the API
         :rtype: dict
@@ -45,8 +52,10 @@ class TerminalClientAPI(PayStackBaseClientAPI):
         return self._post_request("/terminal/commission_device", data=data)
 
     def decommission_terminal(self, serial_number: str) -> dict:
-        """Deactivate debug device by unlinking it from your integration
-        :param serial_number: The serial number of the device
+        """
+        Deactivate debug device by unlinking it from your integration
+
+        :param: serial_number: The serial number of the device
 
         :return: The response from the API
         :rtype: dict
@@ -55,12 +64,14 @@ class TerminalClientAPI(PayStackBaseClientAPI):
         return self._post_request("/terminal/decommission_device", data=data)
 
     def update_terminal(
-        self, terminal_id: str, terminal_name: str, terminal_address: str
+            self, terminal_id: str, terminal_name: str, terminal_address: str
     ) -> dict:
-        """Update details of a terminal
-        :param terminal_id: The terminal iD the event is sent to
-        :param terminal_name: Name of the terminal
-        :param terminal_address: Address of the terminal
+        """
+        Update details of a terminal
+
+        :param: terminal_id: The terminal iD the event is sent to
+        :param: terminal_name: Name of the terminal
+        :param: terminal_address: Address of the terminal
 
         :return: The response from the API
         :rtype: dict
@@ -69,9 +80,11 @@ class TerminalClientAPI(PayStackBaseClientAPI):
         return self._put_request(f"/terminal/{terminal_id}", data=data)
 
     def fetch_event_status(self, terminal_id: str, event_id: str) -> dict:
-        """Fetch details of a specific event status sent to the terminal
-        :param terminal_id: iD of the terminal the event is sent to
-        :param event_id: The event id sent to the terminal
+        """
+        Fetch details of a specific event status sent to the terminal
+
+        :param: terminal_id: iD of the terminal the event is sent to
+        :param: event_id: The event id sent to the terminal
 
         :return: The response from the API
         :rtype: dict
@@ -79,8 +92,10 @@ class TerminalClientAPI(PayStackBaseClientAPI):
         return self._get_request(f"/terminal/{terminal_id}/event/{event_id}")
 
     def fetch_terminal_status(self, terminal_id: str) -> dict:
-        """Fetch the availability of a terminal before sending an event
-        :param terminal_id: The terminal iD the event is sent to
+        """
+        Fetch the availability of a terminal before sending an event
+
+        :param: terminal_id: The terminal iD the event is sent to
 
         :return: The response from the API
         :rtype: dict
@@ -88,15 +103,17 @@ class TerminalClientAPI(PayStackBaseClientAPI):
         return self._get_request(f"/terminal/{terminal_id}/presence")
 
     def list_terminals(
-        self,
-        per_page: int,
-        next_cursor: Optional[str] = None,
-        previous_cursor: Optional[str] = None,
+            self,
+            per_page: int,
+            next_cursor: Optional[str] = None,
+            previous_cursor: Optional[str] = None,
     ) -> dict:
-        """List the Terminals available on your integration
-        :param per_page: The number of records to return. Default value is 50
-        :param next_cursor:
-        :param previous_cursor:
+        """
+        List the Terminals available on your integration
+
+        :param: per_page: The number of records to return. Default value is 50
+        :param: next_cursor:
+        :param: previous_cursor:
 
         :return: The response from the API
         :rtype: dict
@@ -105,8 +122,10 @@ class TerminalClientAPI(PayStackBaseClientAPI):
         return self._get_request("/terminal", params=params)
 
     def fetch_terminal(self, terminal_id: str) -> dict:
-        """Get the details of a terminal
-        :param terminal_id: The terminal iD the event is sent to
+        """
+        Get the details of a terminal
+
+        :param: terminal_id: The terminal iD the event is sent to
 
         :return: The response from the API
         :rtype: dict
