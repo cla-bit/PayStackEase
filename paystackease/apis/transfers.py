cefbffcd1f7eb1311974d8e02c811fe8dfaa0690
claver <82104865+cla-bit@users.noreply.github.com>
2024-03-19 21:24:11 +0100

    Update transfers.py

--- paystackease/apis/transfers.py
+++ paystackease/apis/transfers.py
@@ -7,10 +7,10 @@ The Transfers API allows you to automate sending money to your customers.
 from datetime import date
 from typing import Optional, List, Dict
 
-from paystackease._base import _PayStackBaseClientAPI
+from paystackease._base import PayStackBaseClientAPI
 
 
-class TransfersClientAPI(_PayStackBaseClientAPI):
+class TransfersClientAPI(PayStackBaseClientAPI):
     """
     Paystack Transfers API
     Reference: https://paystack.com/docs/api/transfer/
