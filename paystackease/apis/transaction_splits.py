0ba87a0ff15b8636b7f65b5f164c2b44e1817fe2
claver <82104865+cla-bit@users.noreply.github.com>
2024-03-19 21:21:32 +0100

    Update transaction_splits.py

--- paystackease/apis/transaction_splits.py
+++ paystackease/apis/transaction_splits.py
@@ -7,10 +7,10 @@ across their payout account, and one or more subaccounts.
 
 from datetime import date
 from typing import Optional, List, Dict, Any
-from paystackease._base import _PayStackBaseClientAPI
+from paystackease._base import PayStackBaseClientAPI
 
 
-class TransactionSplitClientAPI(_PayStackBaseClientAPI):
+class TransactionSplitClientAPI(PayStackBaseClientAPI):
     """
     Paystack Transaction Split API
     Reference: https://paystack.com/docs/api/split/
