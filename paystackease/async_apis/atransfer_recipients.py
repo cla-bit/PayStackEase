29393187473aaba80f49157fca1767847e4e0653
claver <82104865+cla-bit@users.noreply.github.com>
2024-03-19 21:54:21 +0100

    Update atransfer_recipients.py

--- paystackease/async_apis/atransfer_recipients.py
+++ paystackease/async_apis/atransfer_recipients.py
@@ -6,10 +6,10 @@ The Transfer Recipients API allows you to create and manage beneficiaries that y
 
 from datetime import date
 from typing import Optional, Dict, List
-from paystackease._abase import _AsyncPayStackBaseClientAPI
+from paystackease._abase import AsyncPayStackBaseClientAPI
 
 
-class AsyncTransferRecipientsClientAPI(_AsyncPayStackBaseClientAPI):
+class AsyncTransferRecipientsClientAPI(AsyncPayStackBaseClientAPI):
     """
     Paystack Transfer Recipients API
     Reference: https://paystack.com/docs/api/transfer-recipient/
