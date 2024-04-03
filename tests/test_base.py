21769a863cc9fd11e55f2d4968cf2d5c83f9875d
petermbachu.bincom <petermbachu.bincom@gmail.com>
2024-04-03 01:42:12 +0100

    updated the test: abase and base

--- tests/test_base.py
+++ tests/test_base.py
@@ -30,8 +30,8 @@ def test_no_secret_key():
 
 def test_convert_to_string(base_client):
     """Tests for convert to string"""
-    assert base_client._convert_to_string(True) == "true"
-    assert base_client._convert_to_string(False) == "false"
+    assert base_client._convert_to_string(True) == "True"
+    assert base_client._convert_to_string(False) == "False"
     assert base_client._convert_to_string(date.today()) == date.today().strftime("%Y-%m-%d")
     assert base_client._convert_to_string(datetime.today()) == datetime.today().strftime("%Y-%m-%d %H:%M:%S")
 
