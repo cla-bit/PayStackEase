23fc48c1ee71239f493f1b609a45af0fd9e922b7
petermbachu.bincom <petermbachu.bincom@gmail.com>
2024-04-04 22:50:47 +0100

    added a utils file

---
+++ paystackease/_utils.py
@@ -0,0 +1,13 @@
+""" Utility functions for paystackease """
+
+from typing import NamedTuple, Optional, Union, Dict, List, Any
+
+
+class Response(NamedTuple):
+    """
+    Paystack API Response from the server after HTTP requests have been made
+    """
+    status_code: int
+    status: str
+    message: str
+    data: Optional[Union[Dict[str, Any], List[Any]]]
